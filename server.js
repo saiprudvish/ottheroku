54e00f409284d3b18ccdf1cef17887f855e9477f
saiprudvish <rsaiprudvish123@gmail.com>
2022-01-14 17:46:16 +0530

    gg

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use("/product",productApi)
 //invalid path
 app.use((req, res, next) => {
 
-    res.send({ message: `path ${req.url} is invalid` })
+    res.send({ message: `path ${req.url} is invaid` })
 })
 
 //error handling middleware
